15a8ff081ac1d0ce8b72e9464fbd0c71128d5234
shenshen <xiaoguaishou404@qq.com>
2023-03-08 17:33:26 +0800

    主界面搭建

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.scss'
-import Home from './view/Home.vue'
-import Doc from './view/Doc.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./index.scss";
+import { router } from './router';
 
-import { createRouter, createWebHashHistory } from 'vue-router'
-const history = createWebHashHistory()
-const router = createRouter({
-    history,
-    routes: [
-        {
-            path: '/',
-            name: 'Home',
-            component: Home
-        }, {
-            path: '/doc',
-            name: 'Doc',
-            component: Doc
-        }
-    ]
-})
-
-createApp(App).use(router).mount('#app')
-
-
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
